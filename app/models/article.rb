579148e756ec40eab3b62606528e112d2f575b20
kkekk <yeongeechong@gmail.com>
2018-12-27 20:41:48 +0100

    tag feature complete!

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,18 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list=(tags_string)
+    # get names from string
+    tag_names = tags_string.split(",").map{|s| s.strip.downcase}.uniq
+    new_or_found_tags = tag_names.map { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
+
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
 end
