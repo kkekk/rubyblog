4d4398c29bc8da417d9cbe0aed6784b6537bdee0
kkekk <yeongeechong@gmail.com>
2018-12-27 14:42:32 +0100

    Form for posts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,38 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+    find_article
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  def edit
+    find_article
+  end
+
+  def update
+    find_article
+    @article.update article_params
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    find_article
+    @article.destroy
+    redirect_to articles_path
   end
 end
